0694b0a0c9c3511d55871349b1dd7c4cd856e7fb
movlonboevyahyo-a11y <movlonboevyahyo@gmail.com>
2026-03-06 19:40:03 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,55 @@
+const topics = {
+
+grammar: [
+"Ot so'z turkumi",
+"Fe'l zamonlari",
+"Sifat darajalari",
+"Olmosh turlari",
+"Ravish turlari",
+"Bog'lovchilar",
+"Ko'makchilar",
+"Yuklamalar",
+"Sintaksis asoslari",
+"Gap bo'laklari"
+],
+
+science: [
+"Ilmiy uslub xususiyatlari",
+"Matn tahlili usullari",
+"Dalillash va isbot",
+"Ilmiy terminlar",
+"Abstrakt tushunchalar",
+"Ilmiy maqola tuzilishi",
+"Xulosa chiqarish",
+"Statistik ma'lumotlardan foydalanish",
+"Tadqiqot metodlari",
+"Ilmiy nutq"
+],
+
+literature: [
+"Badiiy tasvir vositalari",
+"Metafora",
+"Epitet",
+"Taqqoslash",
+"Personifikatsiya",
+"Badiiy matn tahlili",
+"Adabiy janrlar",
+"Lirik asarlar",
+"Nasriy asarlar",
+"Dramatik asarlar"
+],
+
+dictionary: [
+"Sinonimlar",
+"Antonimlar",
+"Paronimlar",
+"Frazeologizmlar",
+"Arxaizmlar",
+"Neologizmlar",
+"Dialekt so'zlar",
+"O'zlashma so'zlar",
+"Terminlar",
+"Ko'p ma'noli so'zlar"
+]
+
+};
